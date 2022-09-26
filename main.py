929548ae1d19099f6445eeaae15fa5c4cea4015a
sebbyolive <97519992+sebbyolive@users.noreply.github.com>
2022-09-26 16:19:13 +0100

    update

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ import time
 import os
 
 class Player:
-    def __init__(self, name: str, guess = 0) -> None:
-        self.name = name
+    def __init__(self, guess = 0) -> None:
         self.guess = guess
 
-    def player_name(self):
-        self.name = input("What is your name? ")
-        return self.name
-
     def value_guess(self):
-        self.guessguess = input("What is your final value guess? ")
+        self.guess = input("What is your final value guess? ")
         return self.guess
 
 class Deck:
-    def __init__(self, deck = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")) -> None:
+    def __init__(self, deck = requests.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")) -> None:
         self.deck = deck
         self.deck_info = self.deck.json()
 
@@ -27,12 +22,6 @@ class Deck:
     def num_cards_remaining(self):
         self.cardsremaining = self.deck_info["remaining"]
         return self.cardsremaining
-
-deck = Deck()
-deck_id = deck.find_deck_id()
-cards_remaining = deck.num_cards_remaining()
-print(deck_id)
-print(cards_remaining)
 
 class GameBrain:
     def __init__(self, difficulty: str = "") -> None:
@@ -71,10 +60,29 @@ class GameBrain:
         elif difficulty == "2": 
             return 5
         elif difficulty == "3": 
-            return 7
+            return 2
         elif difficulty == "off": exit()
 
     def deal_player_in(self):
+        draw_two_cards = requests.get(f"https://deckofcardsapi.com/api/deck/{deck.find_deck_id()}/draw/?count=4")
+        cards_drawn = draw_two_cards.json()
+        card1 = cards_drawn["cards"][0]["value"]
+        card2 = cards_drawn["cards"][1]["value"]
+        card3 = cards_drawn["cards"][2]["value"]
+        card4 = cards_drawn["cards"][3]["value"]
+        cards_remaining = cards_drawn["remaining"]
+        return card1, card2, card3, card4, cards_remaining
+
+    def player_cards_value_calc(self, *args):
+        pc_value = 0
+        low_cards = ["2", "3","4", "5", "6"]
+        high_cards = ["10", "JACK","QUEEN", "KING", "ACE"]
+        for i in args: 
+            if i in low_cards: pc_value +=1
+            elif i in high_cards: pc_value -=1
+        return pc_value
+
+    def deal_dealer_in(self):
         draw_two_cards = requests.get(f"https://deckofcardsapi.com/api/deck/{deck.find_deck_id()}/draw/?count=2")
         cards_drawn = draw_two_cards.json()
         card1 = cards_drawn["cards"][0]["value"]
@@ -82,44 +90,49 @@ class GameBrain:
         cards_remaining = cards_drawn["remaining"]
         return card1, card2, cards_remaining
 
-    def player_cards_value_calc(self, card1, card2):
-        pc_value = 0
-        low_cards = ["2", "3","4", "5", "6"]
-        high_cards = ["10", "JACK","QUEEN", "KING", "ACE"]
-        if card1 in low_cards: pc_value += 1
-        elif card1 in high_cards: pc_value -= 1
-        if card2 in low_cards: pc_value += 1
-        elif card2 in high_cards: pc_value -= 1
-        return pc_value
+    def check_player_guess(self, player_guess, total_value):
+        if total_value == player_guess:
+            return True
+        else:
+            return False
 
-    def deal_dealer_in(self):
-        pass
-    def calc_round_value(self, pc_value, c_value):
-        pass
-        
-    def check_player_guess(self, player_guess):
-        pass
 
-class UserList:
-    def __init__(self) -> None:
-        pass
+# GAME CODE BELOW
+gamebrain = GameBrain() 
+deck = Deck()
+deck_id = deck.find_deck_id()
+cards_remaining = deck.num_cards_remaining()
 
-# GAME BELOW
-gamebrain = GameBrain()
-print(gamebrain.welcome_user())
-difficulty = gamebrain.difficulty_choice()
-amount_of_rounds = gamebrain.amount_of_rounds(difficulty)
+print(gamebrain.welcome_user()) # Welcome user
+difficulty = gamebrain.difficulty_choice() # Select difficulty
+
+player1 = Player() # Generate player
 
 for i in range(gamebrain.amount_of_rounds(difficulty)):
-    card1, card2, cards_remaining = gamebrain.deal_player_in()
+    total_value = 0
+    card1, card2, card3, card4, cards_remaining = gamebrain.deal_player_in()
     if cards_remaining < 1:
         print("No more cards!")
         break
     else:
-        print(f"Card1 is {card1}\nCard2 is {card2}\nCards left = {cards_remaining}")
-        print(f"Round value: {gamebrain.player_cards_value_calc(card1, card2)}")
-        gamebrain.round_time(difficulty)
-        os.system("clear")
+        print(f"Your cards are {card1} and {card2}")
+        print(f"Dealer's cards are {card3} and {card4}")
+        print(f"{cards_remaining} cards left")
+        round_value = gamebrain.player_cards_value_calc(card1, card2, card3, card4)
+    total_value += round_value
+    print(round_value)
+    print(total_value)
+    gamebrain.round_time(difficulty)
+    os.system("clear")
+
+player_guess = player1.value_guess()
+print(player_guess)
+result = gamebrain.check_player_guess(player_guess, total_value)
+if result:
+    print("Correct! Well done")
+else: 
+    print(f"Sorry, that is not correct")
+
 
 # Functions needed:
 # Value Guess
