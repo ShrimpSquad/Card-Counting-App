e97fdbd650a8196a2023be23fb15954d55a2e47d
Sebastian Oliver <sebastianoliver@MacBook-Pro.local>
2022-11-04 15:22:49 +0000

    updates

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template("index.html")
 
+@app.route("/home", methods = ["GET", "POST"])
+def home():
+    return render_template("home.html", myvar = "Seb")
 if __name__ == "__main__":
     app.run(debug = True)
